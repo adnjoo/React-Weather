ad11f219721d5703b1000fbe10f68377e201ad30
Andrew Njoo <adnjoo@gmail.com>
2021-07-20 00:07:56 -0700

    edit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Weather from "./components/weather";
 import Footer from "./components/footer";
 
+import { Helmet } from 'react-helmet';
+
 // font awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -12,11 +14,14 @@ library.add(fab)
 function App() {
   return (
     <div>
+      <Helmet>
+        <style>{'body { background-color: beige; }'}</style>
+      </Helmet>
       <div
         className="border border-l-8 border-blue-500 shadow rounded-lg
         py-2 px-8 m-10 md:m-20 mt-20
         grid justify-center
-        bg-gradient-to-r from-blue-50 to-blue-100"
+        bg-gradient-to-r from-blue-100 to-blue-300"
         >
         <p className='text-3xl'>My weather ☀️☁️</p>
         <Weather />
