ebee38e93c8132e445571acaf4c1d77e635d304a
Andrew Njoo <adnjoo@gmail.com>
2021-07-22 18:12:36 -0700

    edit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Favicon from 'react-favicon'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Favicon url='https://raw.githubusercontent.com/adnjoo/weather-app/master/weather.ico' />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
