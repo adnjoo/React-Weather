ffbfdd306ea41f2b3c9c7b103a43a82e1a36a1ff
Andrew Njoo <adnjoo@gmail.com>
2021-07-27 22:34:24 -0700

    edit

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer = () => {
   return (
-  <div style={{textAlign:'center'}} className='mt-64 md:mt-48'>
+  <div style={{textAlign:'center'}} className='mt-64 md:mt-48 mb-8'>
     made by <a href='http://github.com/adnjoo'>adnjoo&nbsp;
     <FontAwesomeIcon icon={['fab','github']}/>
     </a>
